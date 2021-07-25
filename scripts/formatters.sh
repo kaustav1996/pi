a313ec3684510bd2f0e7245448d2cf75e824183e
Kaustav Banerjee <kaustav@setu.co>
2021-07-25 11:05:07 +0530

    First Commit

---
+++ scripts/formatters.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+_BOLD_="\e[1m"
+_UNBOLD_="\e[22m"
+
+_RED_="\e[31m"
+_GREEN_="\e[32m"
+_YELLOW_="\e[33m"
+_BLUE_="\e[34m"
+_MAGENTA_="\e[35m"
+_CYAN_="\e[36m"
+
+_RESETALL_="\e[0m"
---
+++ ssh_pi.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+ssh 'pi@192.168.31.138'
