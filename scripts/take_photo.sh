2eed4e110dd8770b35d8025a74db76f848c9a20b
Kaustav Banerjee <kaustav@setu.co>
2021-07-25 11:24:21 +0530

    wip

--- scripts/take_photo.sh
+++ scripts/take_photo.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-raspistill -o ../static/img/image.jpg
+# Enable formatting modifiers
+. scripts/formatters.sh
+
+image_number="$(cat camera/.image_number 2>/dev/null)"
+
+printf "Curren Image Number: ${_GREEN_}${_BOLD_}$image_number${_UNBOLD_}${_RESETALL_}\n"
+
+raspistill -o ~/pi/static/img/image_${image_number}.jpg
+
+printf "\n"
+
+raspistill -o ~/pi/static/img/image.jpg
